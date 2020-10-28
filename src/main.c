2116391209478e0570d487ec4cac86f6dbe7eae5
Waged <abdelwagedmohamed@gmail.com>
2020-10-28 20:05:50 +0100

    binary semaphore

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "user.h"
 
 User user;
-xSemaphoreHandle mutexBus;
+xSemaphoreHandle binSemaphorHandle;
 
 void updateUser(char *name, char *address, int age)
 {
@@ -18,49 +18,35 @@ void updateUser(char *name, char *address, int age)
 
 void task1(void *params)
 {
-    // int counter = 0;
+   
     while (1)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         printf("\n%s running \n", (char *)params);
         updateUser("Tailor", "China", 32);
-        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
-        {
-            setUser(user);
-            User *getuser = getUser();
-            printf("name1: %s, ", getuser->name);
-            printf("addr1: %s, ", getuser->address);
-            printf("age1: %d\n", getuser->age);
-            xSemaphoreGive(mutexBus);
-        }
-        else
-        {
-            printf("\n couldn't set user from Task 1 \n");
-        }
+        setUser(user);
+        User *getuser = getUser();
+        printf("name1: %s, ", getuser->name);
+        printf("addr1: %s, ", getuser->address);
+        printf("age1: %d\n", getuser->age);
+        xSemaphoreGive(binSemaphorHandle);
     }
 }
 
 void task2(void *params)
 {
-    // int counter = 0;
     while (1)
     {
+        xSemaphoreTake(binSemaphorHandle, portMAX_DELAY);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
         printf("\n%s running. \n", (char *)params);
         updateUser("James", "USA", 26);
-        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
-        {
-            setUser(user);
-            User *getuser = getUser();
-            printf("name2: %s, ", getuser->name);
-            printf("addr2: %s, ", getuser->address);
-            printf("age2: %d\n", getuser->age);
-            xSemaphoreGive(mutexBus);
-        }
-        else
-        {
-            printf("\n couldn't set user from Task 2 \n");
-        }
+        setUser(user);
+        User *getuser = getUser();
+        printf("name2: %s, ", getuser->name);
+        printf("addr2: %s, ", getuser->address);
+        printf("age2: %d\n", getuser->age);
+ 
     }
 }
 
@@ -73,7 +59,8 @@ void app_main()
     // send parameters NULL for no passing
     // piriority 1 lowest  5 highest
     // update modify delete this task
-    mutexBus = xSemaphoreCreateMutex();
+    // mutexHandle = xSemaphoreCreateMutex();  //timeout relaese
+    binSemaphorHandle = xSemaphoreCreateBinary();              //logic  release
     xTaskCreate(&task1, "counter 1", 2048, "Task 1", 2, NULL); //passing parameter task 1
     xTaskCreate(&task2, "counter 2", 2048, "Task 2", 2, NULL); //passing parameter task 2
     // xTaskCreatePinnedToCore(&task2, "counter 2", 2048, "task 2", 2, NULL, 1); //create task in the second core of ESP32  - 2 core capability
