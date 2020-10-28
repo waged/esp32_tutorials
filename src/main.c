5c0d6f5a3384e44ccfe07756e6e4b15cb053131d
Waged <abdelwagedmohamed@gmail.com>
2020-10-28 02:02:37 +0100

    mutex

--- src/main.c
+++ src/main.c
@@ -3,40 +3,67 @@
 #include <string.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/semphr.h>
 #include "user.h"
 
-void updateUser(User *user)
+User user;
+xSemaphoreHandle mutexBus;
+
+void updateUser(char *name, char *address, int age)
 {
-    strcpy(user->name, "Tailor");
-    strcpy(user->address, "China");
-    user->age = 35;
+    strcpy(user.name, name);
+    strcpy(user.address, address);
+    user.age = age;
 }
 
-void task1(void* params)
+void task1(void *params)
 {
-    int counter = 0;
-    User user;
-    updateUser(&user);
-    setUser(user);
-    User *getuser = getUser();
-    printf("\n\n user_name: %s\n", getuser->name);
-    printf("\n user_address: %s\n", getuser->address);
-    printf("\n user_age: %d\n", getuser->age);
+    // int counter = 0;
     while (1)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("%s counter now equals %d!\n", (char *)params ,++counter);
+        printf("\n%s running \n", (char *)params);
+        updateUser("Tailor", "China", 32);
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            setUser(user);
+            User *getuser = getUser();
+            printf("name1: %s, ", getuser->name);
+            printf("addr1: %s, ", getuser->address);
+            printf("age1: %d\n", getuser->age);
+            xSemaphoreGive(mutexBus);
+        }
+        else
+        {
+            printf("\n couldn't set user from Task 1 \n");
+        }
     }
 }
-void task2(void* params) // never executed!!
+
+void task2(void *params)
 {
-    int counter = 0;
+    // int counter = 0;
     while (1)
     {
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        printf("%s counter now equals %d!\n",(char *)params ,++counter);
+        printf("\n%s running. \n", (char *)params);
+        updateUser("James", "USA", 26);
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            setUser(user);
+            User *getuser = getUser();
+            printf("name2: %s, ", getuser->name);
+            printf("addr2: %s, ", getuser->address);
+            printf("age2: %d\n", getuser->age);
+            xSemaphoreGive(mutexBus);
+        }
+        else
+        {
+            printf("\n couldn't set user from Task 2 \n");
+        }
     }
 }
+
 void app_main()
 {
     //FreeRTOS example to show Multitasking
@@ -46,7 +73,21 @@ void app_main()
     // send parameters NULL for no passing
     // piriority 1 lowest  5 highest
     // update modify delete this task
+    mutexBus = xSemaphoreCreateMutex();
+    xTaskCreate(&task1, "counter 1", 2048, "Task 1", 2, NULL); //passing parameter task 1
+    xTaskCreate(&task2, "counter 2", 2048, "Task 2", 2, NULL); //passing parameter task 2
+    // xTaskCreatePinnedToCore(&task2, "counter 2", 2048, "task 2", 2, NULL, 1); //create task in the second core of ESP32  - 2 core capability
 
-    xTaskCreate(&task1, "counter 1", 2048, "task 1", 2, NULL); //passing parameter task 1
-    xTaskCreate(&task2, "counter 2", 2048, "task 2", 2, NULL); //passing parameter task 2
+    /*
+    FreeRTOS interaction between 2 or more tasks
+                        I want to communicate directly to another task.care about counting or sending simple data --YES--> Task Notification
+                        || NO
+                        Someone needs to block or unblock a task, I don't care who      -- Yes--> Semaphore 
+                        || NO
+                        Only the task that blocks another task can unblock it           --YES-->   Mutex    
+                        || NO
+                        I need to pass data from one task to another                    --YES-->   Queue
+                        || NO
+                        Mulltiple things need to happen before I unblock a task         --YES--> Event Group
+    */
 }
