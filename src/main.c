0f3453bf1b60bca25cabf373ff4a7461d7c42f31
Waged <abdelwagedmohamed@gmail.com>
2020-10-28 21:01:29 +0100

    queue example

--- src/main.c
+++ src/main.c
@@ -7,8 +7,7 @@
 #include "user.h"
 
 User user;
-xSemaphoreHandle binSemaphorHandle;
-
+xQueueHandle qHandler;
 void updateUser(char *name, char *address, int age)
 {
     strcpy(user.name, name);
@@ -18,63 +17,51 @@ void updateUser(char *name, char *address, int age)
 
 void task1(void *params)
 {
-   
+    int count = 0;
     while (1)
     {
+        count++;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("\n%s running \n", (char *)params);
-        updateUser("Tailor", "China", 32);
-        setUser(user);
-        User *getuser = getUser();
-        printf("name1: %s, ", getuser->name);
-        printf("addr1: %s, ", getuser->address);
-        printf("age1: %d\n", getuser->age);
-        xSemaphoreGive(binSemaphorHandle);
+        long ok = xQueueSend(qHandler, &count, 1000 / portTICK_PERIOD_MS);
+        if (ok)
+        {
+            printf("\n%s added msg to queue \n", (char *)params);
+            updateUser("Tailor", "China", 32);
+            setUser(user);
+            User *getuser = getUser();
+            printf("name1: %s, ", getuser->name);
+            printf("addr1: %s, ", getuser->address);
+            printf("age1: %d\n", getuser->age);
+        }
+        else
+        {
+            printf("%s :Failed to add message to Queue\n", (char *)params);
+        }
     }
 }
-
 void task2(void *params)
 {
     while (1)
     {
-        xSemaphoreTake(binSemaphorHandle, portMAX_DELAY);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        printf("\n%s running. \n", (char *)params);
+        int rxCount;
+        if (xQueueReceive(qHandler, &rxCount, 5000 / portTICK_PERIOD_MS))
+        {
+        printf("\n%s running and got number %d \n", (char *)params, rxCount);
         updateUser("James", "USA", 26);
         setUser(user);
         User *getuser = getUser();
         printf("name2: %s, ", getuser->name);
         printf("addr2: %s, ", getuser->address);
         printf("age2: %d\n", getuser->age);
- 
+        }
+      
     }
 }
 
 void app_main()
 {
-    //FreeRTOS example to show Multitasking
-
-    //friendly name of the task
-    // how much memory you want to keep it for this task
-    // send parameters NULL for no passing
-    // piriority 1 lowest  5 highest
-    // update modify delete this task
-    // mutexHandle = xSemaphoreCreateMutex();  //timeout relaese
-    binSemaphorHandle = xSemaphoreCreateBinary();              //logic  release
+    qHandler = xQueueCreate(3, sizeof(int));
     xTaskCreate(&task1, "counter 1", 2048, "Task 1", 2, NULL); //passing parameter task 1
     xTaskCreate(&task2, "counter 2", 2048, "Task 2", 2, NULL); //passing parameter task 2
     // xTaskCreatePinnedToCore(&task2, "counter 2", 2048, "task 2", 2, NULL, 1); //create task in the second core of ESP32  - 2 core capability
-
-    /*
-    FreeRTOS interaction between 2 or more tasks
-                        I want to communicate directly to another task.care about counting or sending simple data --YES--> Task Notification
-                        || NO
-                        Someone needs to block or unblock a task, I don't care who      -- Yes--> Semaphore 
-                        || NO
-                        Only the task that blocks another task can unblock it           --YES-->   Mutex    
-                        || NO
-                        I need to pass data from one task to another                    --YES-->   Queue
-                        || NO
-                        Mulltiple things need to happen before I unblock a task         --YES--> Event Group
-    */
 }
