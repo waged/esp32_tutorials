d532928e8e35724505ee41115c54a1dc404e256a
Waged <abdelwagedmohamed@gmail.com>
2020-12-29 21:19:38 +0100

    commenting for BLE

--- src/main.c
+++ src/main.c
@@ -14,14 +14,19 @@
 #include "cJSON.h"
 #include "sdkconfig.h"
 
+#define DEVICE_INFO_SERVICE 0x180A
+#define MANUFACTURER_NAME_CHAR 0x2A29
+#define BLE_NAME "IoT-BLE"
+
 char *TAG = "BLE-CONNECTION";
-#define BLE_NAME "WEGZ-BLE"
+
 uint8_t ble_addr_type;
+
+char* wifi_ssid ;
+char* wifi_psk ;
 
 void ble_app_advertise(void);
 
-#define DEVICE_INFO_SERVICE 0x180A
-#define MANUFACTURER_NAME_CHAR 0x2A29
 
 static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
@@ -31,16 +36,16 @@ static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_g
     cJSON *payload = cJSON_Parse(incoming_data);
     cJSON *ssid = cJSON_GetObjectItem(payload, "SSID");
     cJSON *psk = cJSON_GetObjectItem(payload, "PSK");
-    printf("WiFi Credentials SSID:(%s) & PSK: (%s)\n", ssid->valuestring, psk->valuestring);
+    wifi_ssid = ssid->valuestring;
+    wifi_psk = psk->valuestring;
+    printf("WiFi Credentials SSID:(%s) & PSK: (%s)\n", wifi_ssid, wifi_psk);//ssid->valuestring, psk->valuestring);
     return 0;
 }
-
 static int device_info(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     printf("BLE RECV MSG: %.*s \n", ctxt->om->om_len, ctxt->om->om_data);
     return 0;
 }
-
 static const struct ble_gatt_svc_def gatt_svcs[] = {
     {.type = BLE_GATT_SVC_TYPE_PRIMARY,
      .uuid = BLE_UUID16_DECLARE(DEVICE_INFO_SERVICE),
@@ -105,8 +110,8 @@ void ble_app_on_sync(void)
     // ble_addr_t addr;
     // ble_hs_id_gen_rnd(1, &addr);
     // ble_hs_id_set_rnd(addr.val);
-    ble_hs_id_infer_auto(0, &ble_addr_type);
-    ble_app_advertise();
+    ble_hs_id_infer_auto(0, &ble_addr_type); // determines automatic address.
+    ble_app_advertise();  //start advertising the services -->
 }
 
 void host_task(void *param)
@@ -118,13 +123,13 @@ void host_task(void *param)
 void app_main()
 {
     nvs_flash_init();
-    esp_nimble_hci_and_controller_init();
-    nimble_port_init();
-    ESP_ERROR_CHECK(ble_svc_gap_device_name_set(BLE_NAME));
-    ble_svc_gap_init();
-    ble_svc_gatt_init();
-    ble_gatts_count_cfg(gatt_svcs);
-    ble_gatts_add_svcs(gatt_svcs);
-    ble_hs_cfg.sync_cb = ble_app_on_sync;
+    esp_nimble_hci_and_controller_init();   //initialize bluetooth controller.
+    nimble_port_init(); //nimble library initialization.
+    ESP_ERROR_CHECK(ble_svc_gap_device_name_set(BLE_NAME)); //set BLE name. 
+    ble_svc_gap_init(); //initialize the gap service.
+    ble_svc_gatt_init(); //initailize the gatt service.
+    ble_gatts_count_cfg(gatt_svcs); // config all the gatt services that wanted to be used.
+    ble_gatts_add_svcs(gatt_svcs);  // queues all services.
+    ble_hs_cfg.sync_cb = ble_app_on_sync; 
     nimble_port_freertos_init(host_task);
 }
